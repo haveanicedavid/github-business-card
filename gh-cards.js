a88ba470ea1acf4945e6093fd4f33a0b086d4bd2
David Daniel <davedan7@gmail.com>
2015-09-17 14:58:39 -0600

    Form up

--- gh-cards.js
+++ gh-cards.js
@@ -2,40 +2,47 @@ UserCards = new Mongo.Collection('userCards');
 
 if (Meteor.isServer) {
 
-  Meteor.publish('userCards', function() {
-    if (this.userId) {
-      return Meteor.users.find(
-          {_id: this.userId}
-        );
-    } else {
-      this.ready();
-    }
-  });
+  // Meteor.publish('userCards', function() {
+  //   if (this.userId) {
+  //     return Meteor.users.find(
+  //         {_id: this.userId}
+  //       );
+  //   } else {
+  //     this.ready();
+  //   }
+  // });
 }
 
 if (Meteor.isClient) {
-  Meteor.subscribe('userCards');
+  // Meteor.subscribe('userCards');
 
   Template.body.helpers({
     userCards: function() {
       return UserCards.find({});
+    },
+
+    cardMade: function() {
+      var card = UserCards.findOne({owner: Meteor.userId()});
+      return card ? true : false;
     }
   });
 
   Template.body.events({
     'click .new-user': function () {
-      Meteor.call('fetchUserData');
+      Meteor.call('fetchUserData', this._id);
     }
   });
 }
 
 Meteor.methods({
-  fetchUserData: function() {
+  fetchUserData: function(id) {
     var token = Meteor.user().services.github.accessToken;
     var url = 'http://api.github.com/user?access_token=' + token;
 
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
+    } else if (UserCards.findOne({owner: Meteor.userId()})) {
+      throw new Meteor.Error('card already exists');
     }
 
     HTTP.get(url, function(error, result) {
@@ -51,6 +58,8 @@ Meteor.methods({
   createCard: function(userData) {
     UserCards.insert({
       owner: Meteor.userId(),
+      name: userData.name,
+      location: userData.location,
       login: userData.login,
       followers: userData.followers,
       following: userData.following,
