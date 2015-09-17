10bbb004661fb90841b24bc7705917cac25c0665
David Daniel <davedan7@gmail.com>
2015-09-16 21:27:33 -0600

    Github APi call finally working... damn you headers

--- gh-cards.js
+++ gh-cards.js
@@ -1,29 +1,5 @@
 UserCards = new Mongo.Collection('userCards');
 
-if (Meteor.isClient) {
-  // counter starts at 0
-  // Session.setDefault('counter', 0);
-  Meteor.subscribe('userCards');
-
-  Template.userData.helpers({
-    // counter: function () {
-    //   return Session.get('counter');
-    // }
-    firstName: function() {
-      // console.log(Meteor.userId());
-      // return Meteor.users;
-    }
-  });
-
-
-  // Template.userData.events({
-  //   'click button': function () {
-  //     // increment the counter when button is clicked
-  //     Session.set('counter', Session.get('counter') + 1);
-  //   }
-  // });
-}
-
 if (Meteor.isServer) {
   // Meteor.startup(function () {
   //   // code to run on server at startup
@@ -38,3 +14,63 @@ if (Meteor.isServer) {
     }
   });
 }
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  // Session.setDefault('counter', 0);
+  Meteor.subscribe('userCards');
+
+  Template.body.events({
+    'click .new-user': function () {
+      // increment the counter when button is clicked
+      // var token = Meteor.user().services.github.accessToken;
+      // HTTP.get('http://api.github.com/user', function(userData) {
+      //   console.log(userData);
+        // Meteor.call('createCard', userData);
+      // });
+      var method = 'GET';
+      var token = Meteor.user().services.github.accessToken;
+      var url = 'http://api.github.com/user?access_token=' + token;
+      // var options = {
+      //   headers: {'Authorization':  token }
+      // };
+      Meteor.call('createCard', method, url, function (error, result) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(result);
+        }
+      });
+    }
+  });
+
+  Template.userCard.helpers({
+    // counter: function () {
+    //   return Session.get('counter');
+    // }
+    firstName: function() {
+      // console.log(Meteor.userId());
+      // return Meteor.users;
+    }
+  });
+}
+
+Meteor.methods({
+  createCard: function(method, url, options) {
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+    // var token = Meteor.user().services.github.accessToken;
+    HTTP.call(method, url, function(error, result) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(result);
+      }
+    });
+
+    // UserCards.insert({
+    //   login: userData.login
+    // });
+  },
+});
